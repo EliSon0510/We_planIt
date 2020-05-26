4ff2a26adee2113a5ea57e9392614e5344ba355f
Florence Carlier <florence.carlier.5@gmail.com>
2020-05-26 18:12:25 +0200

    cloudinary

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,14 +10,12 @@ class ReviewsController < ApplicationController
   def new
     @profile = Profile.find(params[:profile_id])
     @review = Review.new
-    authorize @review
   end
 
   def create
     @review = Review.new(review_params)
     @profile = Profile.find(params[:profile_id])
     @review.profile_id = @profile.id
-    authorize @review
     if @review.save
       redirect_to profile_path(@profile)
     else
@@ -26,12 +24,10 @@ class ReviewsController < ApplicationController
   end
 
   def edit
-    authorize @review
   end
 
   def update
     @review.update(review_params)
-    authorize @review
     if @review.save
       redirect_to profile_path(@review.profile)
     else
@@ -40,7 +36,6 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
-    authorize @review
     @review.destroy
     redirect_to profile_path(@review.profile)
   end
