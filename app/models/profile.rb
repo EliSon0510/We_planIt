81013ae160e7c37a329a7c847276cb81d2ac73ca
Elisabeth Son <elisabeth_son@hotmail.com>
2020-05-26 13:29:45 +0200

    created views for profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,7 +3,7 @@ class Profile < ApplicationRecord
 
   validates :first_name, presence: :true
   validates :last_name, presence: :true
-  validates :gender, presence: :true
+  validates :gender, inclusion: {in: ["male", "female", "other"], allow_nil: false}
 
   has_one_attached :photo
 end
