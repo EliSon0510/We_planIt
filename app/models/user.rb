f2b6551cb7b142241cdb32b73015b337c35e355b
Florence Carlier <florence.carlier.5@gmail.com>
2020-05-27 12:59:09 +0200

    lalala

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :trips
   has_many :interactions
+  has_many :reviews
 
   # this is a test!
 
