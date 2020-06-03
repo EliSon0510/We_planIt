a7eecbfbb55cfee44dd21998c6d257b90cf493c8
Florence Carlier <florence.carlier.5@gmail.com>
2020-06-03 21:07:14 +0200

    design

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,9 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import "../plugins/flatpickr"
+
+
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
